00bb19e168289e34a72f82db7cf21715fd40107a
Keelen Fisher <keelencarrera@gmail.com>
2023-01-27 17:21:27 -0600

    Day 8 completed

---
+++ Day-8/Day8.py
@@ -0,0 +1,167 @@
+# Review: 
+# Create a function called greet(). 
+def greet():
+# Write 3 print statements inside the function.
+  print("Hello...")
+  print("How are you?")
+  print("Are you being the best version of you?")
+# Call the greet() function and run your code.
+greet()
+
+# Function that allows for input
+def greet_with_name(name):
+  print(f"Hello {name}.....")
+  print(f"How are you {name}?")
+
+greet_with_name("Keelen")
+
+# Functions with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"{name} I am from {location}, where are you from?")
+
+greet_with("Keelen", "Houston")
+# Positional Argument, it is important to have the order of the inputs corresponding to the parameters within the function.
+
+# -------------------------------------------------------------------------------------------------------------------------
+
+# You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+import math
+
+def paint_calc(height, width, cover):
+    multi = height * width
+    calculated = multi / cover
+    final_cal = math.ceil(calculated)
+    print(f"You'll need {final_cal} cans of paint.")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+# -------------------------------------------------------------------------------------------------------------------------
+
+# You need to write a function that checks whether if the number passed into it is a prime number or not.
+
+# e.g. 2 is a prime number because it's only divisible by 1 and 2.
+
+# But 4 is not a prime number because you can divide it by 1, 2 or 4.
+
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+ #-------------------------------------------------------------------------------------------------------------------------
+
+# Encryption Code Challenge 1
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+   position = alphabet.index(letter)
+   new_position = position + shift_amount
+   new_letter = alphabet[new_position]
+   cipher_text += new_letter
+  print(f"The encoded text is {cipher_text}")
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(plain_text = text, shift_amount = shift)
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# Encryption Code Challenge 2
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+def decrypt(cypher_text, shift_amount):
+  plain_text = ""
+  for letter in cypher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
+  
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text = text, shift_amount = shift)
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# Encryption Code Challenge 3
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}")
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+
--- Day-8/Day8Notes.md
+++ Day-8/Day8Notes.md
@@ -1,3 +1,21 @@
 # Day 8
 
-## Functions and input
+## Functions with inputs, Arguments and Parameters
+
+### [Code for Day 8](Day8.py)
+
+### Functions with Inputs
+
+- Functions: A way to package complex set of instructions.
+
+- Functions with inputs: A way to ***Pass*** "something" into the package with a complex set of instructions. it's sort of like inserting a USB stick in a computer.
+
+- This "something" is called the ***parameter*** and the assigned something is called the ***argument***
+
+       something (parameter) = 123 (argument)
+
+- ***Positional Argument***, it is important to have the order of the inputs corresponding to the parameters within the function.
+
+- ***Keyword Argument***, assigning each of the parameter name(s) to a certain data type.
+
+### [How to use the list index() method:](https://www.w3schools.com/python/ref_list_index.asp)
